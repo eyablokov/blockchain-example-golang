3281a872be7b696d885ae590863db575d60546dd
Evgeniy Yablokov <eyablokov@me.com>
2018-03-09 19:01:23 +0300

    AddBlock function implemented

--- blockchain.go
+++ blockchain.go
@@ -4,3 +4,10 @@ package main
 type Blockchain struct {
 	block []*Block
 }
+
+// AddBlock saves provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock (data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
