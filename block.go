3131babce4fc2448d9ac14b607cce5d9da3c7afe
Evgeniy Yablokov <eyablokov@me.com>
2018-03-09 19:10:07 +0300

    NewGenesisBlock function implemented

--- block.go
+++ block.go
@@ -36,3 +36,8 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 	block.SetHash()
 	return block
 }
+
+// NewGenesisBlock creates and returns genesis Block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
